8c3ca4ffd955b010915975cef74c804cddd46461
77622353K <77622353K@iespoblenou.org>
2015-10-31 16:14:13 +0100

    Afegit fragment

--- app/src/main/java/com/example/edu/pelis/MainActivity.java
+++ app/src/main/java/com/example/edu/pelis/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
